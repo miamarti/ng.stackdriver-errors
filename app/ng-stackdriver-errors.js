6c8f2893acbd2846111cf4f166760d0ecfcfb341
Miller Augusto <miller.augusto@gmail.com>
2018-01-09 09:55:19 -0200

    Create ng-stackdriver-errors.js

---
+++ app/ng-stackdriver-errors.js
@@ -0,0 +1,53 @@
+window.StackTrace = require('stacktrace-js');
+window.StackdriverErrorReporter = require('stackdriver-errors-js').StackdriverErrorReporter;
+
+(function(window, angular) {
+  'use strict';
+
+  angular.module('ngStackdriverErrors', ['ng']).provider('$exceptionHandler', function() {
+    var $this = {
+      stackDrive: new window.StackdriverErrorReporter(),
+
+      /**
+       * [description]
+       * @param  {[type]} exception [description]
+       * @param  {[type]} cause     [description]
+       * @return {[type]}           [description]
+       */
+      report: function(exception, cause) {
+        $this.stackDrive.report(exception, cause);
+      },
+
+      /**
+       * [description]
+       * @param  {[type]} data [description]
+       * @return {[type]}      [description]
+       */
+      config: function(data) {
+        $this.stackDrive.start(data);
+      },
+
+      /**
+       * [description]
+       * @param  {[type]} userId [description]
+       * @return {[type]}        [description]
+       */
+      setUser: function(userId) {
+        $this.stackDrive.setUser(userId);
+      },
+
+      /**
+       * [description]
+       * @return {[type]} [description]
+       */
+      $get: function() {
+        return {
+          throw: $this.stackDrive.report.bind($this.stackDrive),
+          setUser: $this.stackDrive.setUser.bind($this)
+        };
+      }
+    };
+
+    return $this;
+  });
+})(window, window.angular);
